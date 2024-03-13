b17109357dbb6e06de2eee571f2c8e75e6e5b05d
Christiana Apinoko <122893327+Shvayne@users.noreply.github.com>
2024-03-13 18:24:49 +0100

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 
+def uppercase(str):
+    result = ""
+    for s in str:
+        if 'a' <= s <= 'z':
+            result += chr(ord(s) - 32)
+        else:
+            result += s
+    return result
+
