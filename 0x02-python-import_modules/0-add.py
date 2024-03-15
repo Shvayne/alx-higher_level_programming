1de49f9415abfa46e0ae77943809ab0fc6769094
Christiana Apinoko <christyapinoko@gmail.com>
2024-03-15 00:31:15 -0700

    created README.md and 0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from add_0.py import add
+
+def main():
+    a = 1
+    b = 2
+    solution = add(a, b)
+    print("{} + {} = {}".format(a, b, solution))
+
+if __name__ == "__main__":
+    main()
+
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+ contains projects related to imports and modules in python
