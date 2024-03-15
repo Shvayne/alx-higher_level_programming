e8fe9a7c12344173d1002c24321878e65af6a145
Christiana Apinoko <christyapinoko@gmail.com>
2024-03-15 00:47:21 -0700

    fixed pycodestyle

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,12 +2,14 @@
 
 from add_0 import add
 
+
 def main():
+
     a = 1
     b = 2
     solution = add(a, b)
     print("{} + {} = {}".format(a, b, solution))
 
+
 if __name__ == "__main__":
     main()
-
