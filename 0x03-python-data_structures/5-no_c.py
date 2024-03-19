de301278b08eb3bc2970a441bae27df3116cf4ca
Christiana Apinoko <122893327+Shvayne@users.noreply.github.com>
2024-03-19 21:37:53 +0100

    correct some files

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    return ''.join([x for x in my_string if x.lower() != 'C'])
+    return ''.join([x for x in my_string if x.lower() != 'c'])
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    max_value = my_list[0]
     if not my_list:
         return None
+    max_value = my_list[0]
     for digit in my_list:
         if digit > max_value:
             max_value = digit
