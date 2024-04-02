fa0ed0e4dde6f4cb51c8fa753522026a24b8a68e
Christiana Apinoko <122893327+Shvayne@users.noreply.github.com>
2024-04-02 16:54:50 +0100

    remove tests

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -13,18 +13,3 @@ def safe_print_integer_err(value):
         sys.stderr.write("Exception: " + str(e))
         return False
 
-
-value = 89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
