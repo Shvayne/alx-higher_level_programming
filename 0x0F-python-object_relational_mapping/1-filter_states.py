f6693ff51e870a4a34b71d4590f6abe59ae87333
Christiana Apinoko <christyapinoko@gmail.com>
2024-06-23 13:44:56 -0700

    script to print states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A script that lists all states with a name starting with N"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=db_name
+            )
+    cursor = db.cursor()
+    cursor.execute(SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC)
+    rows = cursor.fetchall()
+    for row in rows:
+        print (row)
+
+    cursor.close()
+    db.close()
