e34ea8a7438710a6935ec18ec7410d6117c93553
Christiana Apinoko <christyapinoko@gmail.com>
2024-07-11 08:03:39 -0700

    correct bug

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(sys.argv[1], sys.argv[2], sys.argv[3])
     engine = create_engine(url)
     Session = sessionmaker(bind=engine)
+    session = Session()
 
     state_name = argv[4]
     state = session.query(State).filter(State.name == state_name).first()
