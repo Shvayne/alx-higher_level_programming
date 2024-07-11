8be1cccd4a8f56646869a9f2d47397b8e45d656f
Christiana Apinoko <christyapinoko@gmail.com>
2024-07-11 05:28:16 -0700

    correct importation of rectangle class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """a square module"""
-from rectangle import Rectangle
+from models.rectangle import Rectangle
+
 class Square(Rectangle):
     """Square class inheriting from rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
