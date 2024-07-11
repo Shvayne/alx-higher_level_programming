0b4c0922c3c85b3a2c9121b1c0ffd7ade9006811
Christiana Apinoko <christyapinoko@gmail.com>
2024-07-11 06:16:09 -0700

    document module

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,18 +4,15 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    owner = sys.argv[1]
-    repo = sys.argv[2]
-    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
-    response  = requests.get(url)
-    if response.status_code == 200:
-        commits = response.json()[:10]
-
-        for commit in commits:
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
-            print(f"{sha}: {author_name}")
-
-        else:
-            print(f"Error fetching commits: {response.status_code}")
+    res = requests.get("https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1]
+        )
+    commits = res.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
 
